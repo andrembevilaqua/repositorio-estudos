b40c03181204c2b9baf93669b3c6144ecae12940
Andre Bevilaqua <andrembevilaqua@gmail.com>
2022-04-23 18:59:34 -0300

    Dino Game Final

--- dino-game/script.js
+++ dino-game/script.js
@@ -1,7 +1,15 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+let vida = document.querySelector("#vida");
+let pontuacao = document.querySelector("#pontuacao");
+let velocidade = 20;
+
+
 let isJumping = false;
 let position = 0;
+vida.textContent = 3;
+pontos = 0;
+pontuacao.textContent = pontos;
 
 function handlKeyUp(event){
     if (event.keyCode == 32){
@@ -37,31 +45,38 @@ function jump(){
 
 function createCactus(){
     const cactus = document.createElement('div');
-    let cactusPosition = 1000;
+    let cactusPosition = 5000;
     let randomTime = Math.random() * 6000;
 
-    console.log(randomTime);
-
     cactus.classList.add('cactus');
-    cactus.style.left = 1000 + 'px';
+    cactus.style.left = 5000 + 'px';
     background.appendChild(cactus);
 
     let leftInterval = setInterval(() => {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+            pontos += 10;
+            pontuacao.textContent  = pontos;
         } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
             //Game Over
+            vida.textContent -= 1;
             clearInterval(leftInterval);
-            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+            background.removeChild(cactus);
+             
+            if (vida.textContent ==    0) {
+                document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+            }
+
         } else {
             cactusPosition -= 10;
-            cactus.style.left = cactusPosition + 'px';
+            cactus.style.left = cactusPosition + 'px';            
         }
-    },20);
+    },20 );
 
     setTimeout(createCactus, randomTime);
 }
 
 createCactus();
 document.addEventListener('keyup', handlKeyUp);
+
